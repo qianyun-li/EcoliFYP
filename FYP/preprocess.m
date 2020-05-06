682c5b744e868d06735435837d87df201d02ac95
GGlin <lgz98616@gmail.com>
2020-05-06 21:23:17 +0800

    Fix resizing bug & Add auto detection button

--- FYP/preprocess.m
+++ FYP/preprocess.m
@@ -1,11 +1,9 @@
 function img = preprocess(img)
-    % Fix image size to 2200 x 2200
-    img = imresize(img, 2200 / size(img,2));
-
     mask = ~(img == 0);
     
     w = 25; img_filtered = img;
-    img_filtered(mask==0) = round(mean(img,'all'));
+%     img_filtered(mask==0) = round(mean(img,'all'));
+    img_filtered(mask==0) = round(sum(img,'all') / sum(mask,'all'));
     img_filtered = roifilt2(img_filtered,mask,@(x) ordfilt2(x,1,ones(w,w),'symmetric'));
     img_filtered = roifilt2(img_filtered,mask,@(x) imfilter(x,ones(w,w)/(w*w),'symmetric'));
     img = imsubtract(img,img_filtered);
