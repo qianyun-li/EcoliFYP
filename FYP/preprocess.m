1fa0b473418e2871ccb35ebcd73c12c434a5bef1
GGlin <lgz98616@gmail.com>
2020-05-23 03:24:32 +0800

    Revised 5.23 (1)
    Remove duplicate contrast enhancing

--- FYP/preprocess.m
+++ FYP/preprocess.m
@@ -7,12 +7,11 @@ img_filtered(mask==0) = round(sum(img,'all') / sum(mask,'all'));
 img_filtered = roifilt2(img_filtered,mask,@(x) ordfilt2(x,1,ones(w,w),'symmetric'));
 img_filtered = roifilt2(img_filtered,mask,@(x) imfilter(x,ones(w,w)/(w*w),'symmetric'));
 img = imsubtract(img,img_filtered);
+
+% Enhance Contrast
 img = roifilt2(img,mask,@(x) imadjust(x,stretchlim(x)));
 
 img = imgaussfilt(img,0.75);
-
-% Enhance Contrast
-img = imadjust(img);
 
 if option(1) == 0
     % Enable Opening
