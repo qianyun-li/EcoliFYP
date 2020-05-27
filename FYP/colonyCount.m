eab576d35a94a1603a4c52682698a447c9d29e86
Jasminqy0 <jasmineqy0@gmail.com>
2020-05-27 12:25:53 +0800

    Update count

--- FYP/colonyCount.m
+++ FYP/colonyCount.m
@@ -10,29 +10,29 @@ function [numCellsMin,numCellsMax]  = colonyCount(bw)
 
     bwWS = bw;
     bwWS(L==0) = 0;
-%     figure, imshow(bw2);
+%     figure, imshow(bwWS);
 
     cc = bwconncomp(bwWS);
     numPixels = cellfun(@numel, cc.PixelIdxList);
 
-%     x = 0:50:max(numPixels);
-%     [f, ~] = ksdensity(numPixels,x);
-%     [~, loc] = findpeaks(f,x);
-    
-%     cellSize = loc(1);
-%     if loc(1) < 200
-%         cellSize = loc(2);
-%     end
-    
     x = 0:50:max(numPixels);
-    [counts, ~] = histcounts(numPixels,x);
-    [~, idx] = max(counts);
-%     cellSize = (x(idx) + x(idx+1)) * 0.5;
-    cellSizeMin = x(idx);
-    cellSizeMax = x(idx+1);
+    [f, ~] = ksdensity(numPixels,x);
+    [~, loc] = findpeaks(f,x);
+    
+    cellSize = loc(1);
+    if loc(1) < 200
+        cellSize = loc(2);
+    end
+    
+%     x = 0:50:max(numPixels);
+%     [counts, ~] = histcounts(numPixels,x);
+%     [~, idx] = max(counts);
+% %     cellSize = (x(idx) + x(idx+1)) * 0.5;
+%     cellSizeMin = x(idx);
+%     cellSizeMax = x(idx+1);
 
-%     cellSizeMin = cellSize * 0.9;
-%     cellSizeMax = cellSize * 1.1;
+    cellSizeMin = cellSize * 0.95;
+    cellSizeMax = cellSize * 1.05;
     
     numCellsMin = fix(allCells / cellSizeMax);
     numCellsMax = fix(allCells / cellSizeMin);
